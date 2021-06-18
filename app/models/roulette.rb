b5243ea31250eb8ce44faf46f7378ac48175db9c
Inuka <seba.nrc@gmail.com>
2021-06-17 23:27:45 -0400

    added spin and get color before create to roulette

--- app/models/roulette.rb
+++ app/models/roulette.rb
@@ -1,3 +1,17 @@
 class Roulette < ApplicationRecord
+  before_create :spin
   has_and_belongs_to_many :players
+
+  def spin
+    random_number = rand(10_000)
+    self.rand_number = random_number
+    self.color = get_color(random_number)
+  end
+
+  def get_color(number)
+    return 'black' if number <= 4900
+    return 'red' if number >= 5100
+
+    'green'
+  end
 end
