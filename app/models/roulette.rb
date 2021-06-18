36e08a1b99b335cb5e51e04e08fab4ffc8c1a840
Inuka <seba.nrc@gmail.com>
2021-06-18 17:02:10 -0400

    added logic to place bets

--- app/models/roulette.rb
+++ app/models/roulette.rb
@@ -1,6 +1,6 @@
 class Roulette < ApplicationRecord
   before_create :spin
-  after_create :place_bet
+  before_create :place_bet
   has_many :bets, dependent: :destroy
   has_many :players, through: :bets
 
@@ -15,5 +15,18 @@ class Roulette < ApplicationRecord
     return 'red' if number >= 5100
 
     'green'
+  end
+
+  def place_bet
+    players = Player.all
+    players.each do |player|
+      bets.new(player_id: player.id, bet_quantity: bet_money(player), bet_color: get_color(rand(10_000)))
+    end
+  end
+
+  def bet_money(player)
+    return player.money if player.money <= 1000
+    return rand(800...player.money) if player.money >= 1001 && player.money <= 1499
+    return rand(800...1500) if player.money >= 1500
   end
 end
