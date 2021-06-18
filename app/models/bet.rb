739488a158526221c82077b717b7b7a34cba4e51
Inuka <seba.nrc@gmail.com>
2021-06-18 17:01:48 -0400

    added logic to play the roulette

--- app/models/bet.rb
+++ app/models/bet.rb
@@ -1,4 +1,23 @@
 class Bet < ApplicationRecord
+  after_create :check_win
+  before_create :discount_money
   belongs_to :roulette
   belongs_to :player
+
+  def check_win
+    green(player) if roulette.color == 'green' && bet_color == roulette.color
+    red_or_black(player) if roulette.color == bet_color
+  end
+
+  def discount_money
+    player.update(money: player.money - bet_quantity)
+  end
+
+  def green(player)
+    player.update(money: bet_quantity * 15)
+  end
+
+  def red_or_black(player)
+    player.update(money: player.money + bet_quantity * 2)
+  end
 end
