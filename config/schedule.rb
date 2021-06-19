5953c17cbe5d185fb0e2f3ae0ffec1e2ae975dbd
Inuka <seba.nrc@gmail.com>
2021-06-19 03:02:31 -0400

    added roulete spin and players refill rake taks to cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,10 @@ env :PATH, ENV['PATH']
 set :output, 'log/cron_log.log'
 set :environment, 'development'
 
-# every 1.minute do
-#  runner 'Roulette.create'
-# end
+every 1.minute do
+  rake 'roulette:spin'
+end
+
+every 1.day, at: '0:00 am' do
+  rake 'players:refill'
+end
