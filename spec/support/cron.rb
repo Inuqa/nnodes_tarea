f80f0d136a9921556ff00dd0575341e05c12ff8e
Inuka <seba.nrc@gmail.com>
2021-06-23 16:10:46 -0400

    test cron http responses

--- spec/support/cron.rb
+++ spec/support/cron.rb
@@ -9,12 +9,22 @@ RSpec.shared_examples 'cron' do
       create_object
       expect { do_request }.to change { variable_output.call }.by(variable_amount)
     end
+
+    it do
+      do_request
+      expect(response).to have_http_status(:ok)
+    end
   end
 
   context 'without header' do
     it 'should change the output' do
       create_object
       expect { do_request }.not_to(change { variable_output.call })
+    end
+
+    it do
+      do_request
+      expect(response).to have_http_status(:internal_server_error)
     end
   end
 end
