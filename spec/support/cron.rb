6475f31261d1cc6804f4a3d3ddec4f56922284f7
Inuka <seba.nrc@gmail.com>
2021-06-23 16:06:37 -0400

    cron test now handles with and without header

--- spec/support/cron.rb
+++ spec/support/cron.rb
@@ -5,9 +5,16 @@ RSpec.shared_examples 'cron' do
   context 'with header' do
     let(:headers) { { CheckCronHeader::CRON_HEADER => true } }
 
-    it 'should refill players money' do
+    it 'should change the output' do
       create_object
       expect { do_request }.to change { variable_output.call }.by(variable_amount)
+    end
+  end
+
+  context 'without header' do
+    it 'should change the output' do
+      create_object
+      expect { do_request }.not_to(change { variable_output.call })
     end
   end
 end
